7fb946dc28e711870b8cd68be0b9429cfa9670a3
brett <bperry13@asu.edu>
2023-07-16 14:38:15 -0500

    Pivoting to image background

--- app/page.js
+++ app/page.js
@@ -3,33 +3,41 @@ import styles from "./page.module.css";
 import "./globals.css";
 import { AiOutlineLinkedin } from "react-icons/ai";
 import { AiOutlineGithub } from "react-icons/ai";
-import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { RiJavascriptFill } from "react-icons/ri";
+import { FaJava } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
+        <div className={styles.welcome}>
           Hey there! I'm glad you're here. I'm Brett, a software engineer based
           in Texas. Have a look around and let me know if you are interested in
           working together.
-        </p>
-      </div>
-      <div className={styles.description}>
+        </div>
         <div className={styles.image}>
           <Image src="/brett.png" alt="Brett Perry" width={300} height={300} />
         </div>
-        <div className={styles.social}>
-          <a href="https://www.linkedin.com/in/brettjp/">
-            <AiOutlineLinkedin alt="LinkedIn" size={50} />
-          </a>
-          <a href="https://www.github.com/bperry13">
-            <AiOutlineGithub alt="GitHub" size={50} />
-          </a>
-          <a href="https://www.instagram.com">
-            <AiOutlineInstagram alt="Instagram" size={50} />
-          </a>
-        </div>
+      </div>
+      <div className={styles.social}>
+        <a href="https://www.linkedin.com/in/brettjp/" target="_blank">
+          <AiOutlineLinkedin alt="LinkedIn" size={50} />
+        </a>
+        <a href="https://www.github.com/bperry13" target="_blank">
+          <AiOutlineGithub alt="GitHub" size={50} />
+        </a>
+        <a href="https://twitter.com/brettperry_dev" target="_blank">
+          <AiOutlineTwitter alt="Twitter" size={50} />
+        </a>
+        <a href="https://www.discord.com" target="_blank">
+          <FaDiscord alt="Discord" size={50} />
+        </a>
       </div>
       <div className={styles.grid}>
         <a href="" className={styles.card}>
@@ -49,7 +57,7 @@ export default function Home() {
           <p>Let me know if you're interested in working together</p>
         </a>
       </div>
-      <div className="footer">
+      <div className={styles.footer}>
         <p>Designed and developed by Brett P.</p>
       </div>
     </main>
