94149a5491c9294e3fd95effdf38e2b7a57548b6
brett <bperry13@asu.edu>
2024-04-02 20:56:10 -0500

    name is scrolling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,24 @@ const config: Config = {
     extend: {
       fontFamily: {
         burtons: "burtons",
-      }
-    },
+      },
+      keyframes: {
+        //Our fall animation keyframes              
+        fall: {
+          '0%': { transform: 'translate(0%,-150%) skewX(0deg)' },
+          '50%': { transform: 'translate(0%,0%) skewX(-10deg)' },
+          '100%': { transform: 'translate(0%,150%) skewX(0deg)' },
+        },
+      },
+      animation: {
+        // You can then reference these keyframes by name in the 
+        // animation section of your theme configuration
+        fall: 'fall 3s ease infinite',
+        // animation shorthand CSS property i.e
+        // animation-name | animation-duration | animation-timing-function 
+        // animation-iteration-count
+      },
+    },  
   },
   plugins: [],
 }
